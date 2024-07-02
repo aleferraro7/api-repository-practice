b04b8145b349076df8411fc05ba21164565502c8
aleferraro7 <[alejandroferraro7@gmail.com]>
2024-07-02 23:45:42 +0200

    users module updated

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,10 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Profile } from 'src/profile/entities/profile.entity';
-import { Repository } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Profile])],
   controllers: [UsersController],
-  providers: [UsersService, Repository],
+  providers: [UsersService],
 })
 export class UsersModule {}
